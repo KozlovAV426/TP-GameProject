de821185bd45b4828ce244446709550424b442c0
KozlovAV426 <44925915+KozlovAV426@users.noreply.github.com>
2019-05-08 23:08:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 
 
 int main() {
-    /*testing::InitGoogleTest();
+    testing::InitGoogleTest();
     testing::InitGoogleMock();
-    RUN_ALL_TESTS();*/
+    RUN_ALL_TESTS();
 
-    std::cout << "Hello! It's a text - strategy. Your aim is to destroy army of your opposite fraction " << std::endl;
+    /*std::cout << "Hello! It's a text - strategy. Your aim is to destroy army of your opposite fraction " << std::endl;
     std::cout << "Select your fraction: 1 - Alliance, 2 - Horde " << std::endl;
     size_t fraction;
     std::cin >> fraction;
     Game game(fraction);
     game.Handle();
-    game.logger_->Analyze();
+    game.logger_->Analyze();*/
 
     return 0;
 
